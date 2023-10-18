c85a8fa79395117e2d9b7ba42f2173aa2f5d72b5
AhmedIzaz <ahmedizazasif@gmail.com>
2023-10-18 10:15:50 +0600

    commented for getBit

--- bit.cpp
+++ bit.cpp
@@ -1,8 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int getBit(int value, int position)
+{
+    // here value 12 = 1100
+    // 1 << 2 =        0100
+    // so & is       = 0100 = 4
+    // otherwise all the position returned 0, that why (!= 0) condition checked. 
+    // (== 1) will not worked cause it will return the value of position, not digit
+    return (value & (1 << position)) != 0;
+};
+
 int main()
 {
-    cout << "bit\n";
+    cout << getBit(12, 2);
     return 0;
 };
