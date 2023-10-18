83c4f5d49a41332d4b8d6c85059dadfea9242b28
AhmedIzaz <ahmedizazasif@gmail.com>
2023-10-18 10:53:18 +0600

    reverseInPosition bit wise problem

--- bit.cpp
+++ bit.cpp
@@ -6,13 +6,24 @@ int getBit(int value, int position)
     // here value 12 = 1100
     // 1 << 2 =        0100
     // so & is       = 0100 = 4
-    // otherwise all the position returned 0, that why (!= 0) condition checked. 
+    // otherwise all the position returned 0, that why (!= 0) condition checked.
     // (== 1) will not worked cause it will return the value of position, not digit
     return (value & (1 << position)) != 0;
 };
 
+int setBit(int value, int position)
+{
+    // it will only set 1 in given position
+    return (value | (1 << position));
+};
+
+int reverseInPosition(int value, int position)
+{
+    return (value ^ (1 << position));
+};
+
 int main()
 {
-    cout << getBit(12, 2);
+    cout << reverseInPosition(12, 2);
     return 0;
 };
