bcb84ef05a0f276aefbf9d2f25665178653a4a40
AhmedIzaz <ahmedizazasif@gmail.com>
2024-02-08 22:33:45 +0600

    cp folder added

---
+++ cp/inclusion.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    const vector<int> li = {1, 2, 3, 4, 6, 7};
+    for(auto i:li){
+        cout << i << endl;
+    };
+    return 0;
+}
