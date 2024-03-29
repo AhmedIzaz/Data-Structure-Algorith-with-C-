80871c09a8d055587145d3f898337b3e99683eca
AhmedIzaz <ahmedizazasif@gmail.com>
2024-03-30 01:35:00 +0600

    keep tracked the last index of subset before adding because of duplicate item checked.

--- learn/subsetOfArrayUsingRecursion/main.cpp
+++ learn/subsetOfArrayUsingRecursion/main.cpp
@@ -1,36 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<vector<int>> subsetOfArrayInt(vector<int> mainArr, int index, vector<vector<int>> subsetResult);
+
+vector<vector<int>> subset(vector<int> mainArr);
 
 int main()
 {
-    vector<int> mainArr = {1, 2, 3};
-    vector<vector<int>> initializer;
-    vector<vector<int>> subsetRes = subsetOfArrayInt(mainArr, 0, initializer);
-
+    vector<int> mainArr = {1, 2, 2};
+    vector<vector<int>> subsetRes = subset(mainArr);
     for(int i = 0 ; i < subsetRes.size(); i++){
         for(int j = 0; j < subsetRes[i].size(); j++){
             cout << subsetRes[i][j];
         }
         cout << endl;
     };
-
     return 0;
 }
 
-
-vector<vector<int>> subsetOfArrayInt(vector<int> mainArr, int index, vector<vector<int>> subsetResult){
-    if(index >= mainArr.size()){
-        return subsetResult;
-    };
-    int subsetResultSize = subsetResult.size();
-    for(int i = 0; i < subsetResultSize; i++){
-        vector<int> indexedItem = subsetResult[i];
-        indexedItem.push_back(mainArr[index]);
-        subsetResult.push_back(indexedItem);
-    };
-    vector<int> indexedArray;
-    indexedArray.push_back(mainArr[index]);
-    subsetResult.push_back(indexedArray);
-    return subsetOfArrayInt(mainArr, index+1, subsetResult);
-};
+vector<vector<int>> subset(vector<int> mainArr){
+    sort(mainArr.begin(), mainArr.end());
+    vector<vector<int>> subsets;
+    subsets.push_back({});
+    int start = 0, ending = 0;
+    for(int i = 0; i < mainArr.size(); i++){
+        if(i > 0 && mainArr[i] == mainArr[(i-1)]) start = ending;
+        else start = 0;
+        ending = subsets.size();
+        for(int j = start; j < ending; j++){
+            subsets.push_back(subsets[j]);
+            subsets.back().push_back(mainArr[i]);
+        }
+    }
+    return subsets;
+}
